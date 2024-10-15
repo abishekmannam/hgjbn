77fed65ed6fce1419a73cd0445f3431e924cbc2e
[Abishek's Air] <[abishek50055@gmail.com]>
2024-10-15 23:42:35 +0530

    basic todo layout with text feild at bottom

--- lib/main.dart
+++ lib/main.dart
@@ -28,11 +28,24 @@ class MyApp extends StatelessWidget{
 
           body: TabBarView(
             children: [
-              Text("Todo list"),
+              actual_app(),
               Text("History"),
             ]
             ),
           
+          floatingActionButton: Row(children: [
+            Expanded(child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: TextField(
+                decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Enter new item',
+              ),
+            ),
+            )),
+
+            ElevatedButton(onPressed: null, child: Icon(Icons.accessibility_new_sharp),)
+          ],),
         ),
 
         
@@ -44,13 +57,30 @@ class MyApp extends StatelessWidget{
 
 }
 
-class actual_app extends StatelessWidget{
+class actual_app extends StatefulWidget{
   actual_app({super.key});
 
   @override
+  State<actual_app> createState() => _actual_appState();
+}
+
+class _actual_appState extends State<actual_app> {
+  var  items = <String>["eat", "Sleep", "work"];
+
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Hello"),
-    );
+    return ListView.builder(
+          itemCount: items.length,
+          prototypeItem: ListTile(
+            title: Text(items.first),
+          ),
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: Icon(Icons.check_box_outline_blank),
+              title: Text(items[index]),
+            );
+          },
+        );
   }
 }
